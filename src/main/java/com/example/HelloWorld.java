b3f55f4d422023e21b818c5fabbf0e8689ad0177
ramyage <2021wa86605@wilp-pilani.ac.in>
2025-07-14 14:30:13 +0530

    Updated HelloWorld.java to trigger Jenkins CI

--- src/main/java/com/example/HelloWorld.java
+++ src/main/java/com/example/HelloWorld.java
@@ -2,6 +2,6 @@ package com.example;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello from Jenkins Maven CI!");
+        System.out.println("CI triggered automatically from Git push!");
     }
 }
